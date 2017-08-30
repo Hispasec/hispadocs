2b4af16741ee4349bae34f9c1b72c2cdd0252646
nekmo <joyague@hispasec.com>
2017-08-30 22:24:21 +0200

    Inputs relative config file.

--- hispadocs/management.py
+++ hispadocs/management.py
@@ -1,4 +1,6 @@
 import click
+import os
+from click import ClickException, FileError
 
 from hispadocs.config import Config
 from hispadocs.odt import OdtFiles, OdtFile
@@ -15,7 +17,13 @@ def cli(ctx, debug):
 @cli.command()
 @click.argument('file')
 def generate(file):
+    if not os.path.lexists(file):
+        raise FileError(file)
+    config_dir = os.path.abspath(os.path.dirname(file))
     config = Config(file)
     config.read()
-    OdtFiles(config['inputs']).create_output(config['output'])
+    if not config.get('inputs'):
+        raise ClickException('Define inputs parameter in config file.')
+    inputs = [os.path.join(config_dir, inp) for inp in config['inputs']]
+    OdtFiles(inputs).create_output(config['output'])
     odt_template(config['output'], config['vars'])
